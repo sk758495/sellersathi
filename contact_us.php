a530b95b2b7c0891c8cee64a3e5819cec355259f
sunny <skvarma758495@gmail.com>
2024-10-08 15:00:57 +0530

    number updated

--- contact_us.php
+++ contact_us.php
@@ -142,6 +142,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     </section>
 
     <!-- Footer Section -->
+     
     <footer class="bg-dark text-light py-5">
         <div class="container">
             <div class="row">
@@ -186,6 +187,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
